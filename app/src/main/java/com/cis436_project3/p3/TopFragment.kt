2a7cb846101964c19806a36833b0808436b1e5bc
andrew <csandrew@umich.edu>
2024-04-10 13:40:41 -0400

    spinner loading cat breeds.

--- app/src/main/java/com/cis436_project3/p3/TopFragment.kt
+++ app/src/main/java/com/cis436_project3/p3/TopFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.lifecycle.ViewModelProvider
 import com.cis436_project3.p3.databinding.FragmentTopBinding
 
@@ -21,8 +23,7 @@ class TopFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-
+    ): View {
         _binding = FragmentTopBinding.inflate(inflater, container, false)
         return binding.root
 
@@ -31,20 +32,18 @@ class TopFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        //alternative to line below
-     //  viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
 
+        val spinner : Spinner? = view.findViewById(R.id.spinnerCats) ?: null
 
-        /*
-        //FOR EXAMPLE FROM LECTURE 9
-        val resultObserver = Observer<Float> {
-            result -> binding.resultText.text = result.toString()
+        viewModel.catBreeds.observe(viewLifecycleOwner) { breeds ->
+            val adapter =
+                ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, breeds)
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner?.adapter = adapter
         }
-        viewModel.getResult().observe(viewLifecycleOwner, resultObserver)
-      //  binding.resultText.text = viewModel.getResult().toString()
 
-         */
+        viewModel.fetchCatBreeds(requireContext())
 
     }
 
